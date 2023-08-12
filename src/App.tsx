0a89f3657d38625d6c29359cc9fa071b4074efc8
Илья Ершов <im.ershov@severstal.com>
2023-08-12 15:19:03 +0200

    React Router, Lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import Counter from "./components/Counter";
-import "./index.scss";
+import { Suspense } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import './index.scss';
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
 
 const App = () => {
-  return (
-    <div className="app">
-      asdasdasdasd
-      <Counter />
-    </div>
-  );
+	return (
+		<div className='app'>
+			<Link to={'/'}>Главная</Link>
+			<Link to={'/about'}>О сайте</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path={'/'} element={<MainPageLazy />} />
+					<Route path={'/about'} element={<AboutPageLazy />} />
+				</Routes>
+			</Suspense>
+		</div>
+	);
 };
 
 export default App;
