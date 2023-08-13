35cf585546a4f58a22237e6524e9ea39a9392fbc
Илья Ершов <im.ershov@severstal.com>
2023-08-13 18:06:48 +0200

    Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
 import './styles/index.scss';
 
 const App = () => {
 	const { theme, toggleTheme } = useTheme();
 	return (
 		<div className={classNames('app', {}, [theme])}>
-			<button onClick={toggleTheme}>TOGGLE</button>
-			<Link to={'/'}>Главная</Link>
-			<Link to={'/about'}>О сайте</Link>
+			<Navbar />
 			<AppRouter />
+			<button onClick={toggleTheme}>TOGGLE</button>
 		</div>
 	);
 };
